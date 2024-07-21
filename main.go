993145e6aba85a6115eefdf3e7ffa8c12501d2d8
acurzon <adam@curzon.org>
2024-07-21 12:37:55 +0100

    feat: add json hello world

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
+		w.Header().Set("Content-Type", "application/json")
+
+		// Create a map to hold the response data
+		response := map[string]string{
+			"message": "Hello World",
+			"time":    time.Now().Format(time.RFC850),
+			"host":    r.Host,
+		}
+
+		json.NewEncoder(w).Encode(response)
 	})
 
 	http.ListenAndServe(":9990", nil)
